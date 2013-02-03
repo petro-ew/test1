458de1ace54bfbf0ce69547c36c9e722f33988e6
petro-ew <v.petroew@gmail.com>
2013-02-03 17:25:06 +0400

    03-02-2013-15-26

--- 13.py
+++ 13.py
@@ -9,3 +9,13 @@ zadacha number 13.
 Написать функцию, возвращающую сумму квадратов элемента списка.
 """
 
+def quadl1 ():
+    l1 = [1, 2, 3, 4, 5]
+    l1 = [x ** 2 for x in l1]
+    print(l1)
+    summ = 0
+    for i in range(len(l1)):
+        summ = summ +  l1[i]
+    print(summ)
+    return []
+quadl1()
