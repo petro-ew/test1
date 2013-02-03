00eae5bec6e4a7e83cde03587a54b64111d2a520
petro-ew <v.petroew@gmail.com>
2013-02-03 22:06:02 +0400

    03-02-2013-15-26

--- 12.py
+++ 12.py
@@ -13,15 +13,21 @@ n = int(input())
 print(n)
 z = n
 zz = n
-k1 = []
+k1 = ()
 #a**2 + b**2 = c**2
 for i in range(1, n+1):
-    a = i**2
+    a2 = i**2
+    a=i
     for j in range(1, z+1):
-        b=j**2
+        b2=j**2
+        b=j
         for k in range(1, zz+1):
-            c=k**2
-            if c == a+b:
-                k1.append(i, j, k)
+            c2=k**2
+            c=k
+            if c2 == a2+b2:
+                k1 = (i, j, k)
                 print(k1)
+           # else:
+           #     print("решений нет!")
+
 
