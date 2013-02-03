63e08ae00fa91b854021939384fe2554aabc30b7
petro-ew <v.petroew@gmail.com>
2013-02-03 18:22:13 +0400

    03-02-2013-15-26

---
+++ 14.py
@@ -0,0 +1,14 @@
+__author__ = 'petro-ew'
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import cmath
+import math
+"""
+14.
+Написать функцию, возвращающую минимальный и максимальный элемент списка в виде кортежа из двух элементов.
+"""
+l1 = [-1, 0, 2, 3, 4, 33, 5, 6, 9, -12]
+k1 = min(l1), max(l1)
+print(l1)
+print(k1)
