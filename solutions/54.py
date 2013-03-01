08b686eeeb95c134396ed1f6eda1f48f4e3ed76a
petro-ew <v.petroew@gmail.com>
2013-03-01 18:25:00 +0400

    54-я delete

--- solutions/54.py
+++ solutions/54.py
@@ -115,6 +115,9 @@ class DailyShedule:
 
     def search(self):
         print("search time метод поиска свободного промежутка времени.")
+        for x in self.sp_time_stop:
+
+
 
     def redo(self):
         print("redo возвращающий список дел, не выполненных в течении дня.")
