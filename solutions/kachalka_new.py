2f19c57a5025b6bbdc8fa69ae63045876e522b84
petro-ew <v.petroew@gmail.com>
2013-03-21 02:20:39 +0400

    connect c baze postgres i sozdanie tablici

--- solutions/kachalka_new.py
+++ solutions/kachalka_new.py
@@ -6,9 +6,11 @@ import sys
 import time
 import os
 import subprocess
+import string
 import shlex
 import os.path
 some_dir ='/home/petro-ew/work/2013/'
+some_dir ='d:\\py_work\\test1\\solutions\\'
 some_dir2='/home/petro-ew/work/crx2rnx'
 some_dir_all = '/home/petro-ew/work/crx2rnx/txt/all/'
 zz_year=2013
@@ -16,12 +18,21 @@ year_fn = 13; # последние две цифры нужного вам го�
 #fday  #first day
 #lday #last day
 #cday # новый\следующий current day
+sta = "brap"
+fdata = 76
+ldata = 78
+"""
 sta = sys.argv[1] #название станции
 fdata = int(sys.argv[2]) #first data вводится как первый аргумент  (начало расчета)
 ldata = int(sys.argv[3]) #last data вводится как второй аргумент (конец расчета)
+"""
 print(time.gmtime())
 #print ("\n year=",year," месяц= ",mon, " день= ", mday, " час= ", hour," минут ", min, " секунд= ", sec,"\n";)
 def execscr(cmd):
+    """
+
+    :param cmd: команда исполняемая
+    """
     args = shlex.split(cmd)
     print(args)
     proc = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
@@ -29,23 +40,83 @@ def execscr(cmd):
     print(out)
 
 
-def fileopen(f_name, simvol):
-    with open(a, "r", encoding="utf-8") as f:
+def fileopen(f_name, curday):
+    """
+
+
+
+    :param curday: дата на которую весь сыр бор
+    :rtype : object
+    :param f_name: имя файла
+    :param simvol: на каком символе ищем переменную
+    :return: переменную
+    """
+    l2  = []
+    s1 = ' .,:;-?!\n'
+    strip = string.whitespace + string.punctuation + s1
+    simvol1 = "IOD slips <  10.0 deg*"
+    simvol2 = "IOD slips >  10.0 deg"
+    simvol3 = "IOD or MP slips <  10.0*"
+    simvol4 = "IOD or MP slips >  10.0"
+
+    with open(os.path.abspath(f_name), "r", encoding="utf-8") as f:
         for line in f:
-            if simvol in line:
-                line = line.strip("\n")
-                print(line)
+            if simvol1 in line:
+                q = line.strip(strip)
+
+                l1 = q.split()
+                z = q[:24]
+                cort = (cday, l1[6])
+                l2.insert(1, curday)
+                l2.insert(2, l1[6])
+            #    print(z, l1[6], cday, cort)
+            if simvol2 in line:
+                q = line.strip(strip)
+
+                l1 = q.split()
+                z = q[:24]
+                cort = (cday, l1[6])
+                l2.insert(3, l1[6])
+           #     print(z, l1[6], cday, cort)
+            if simvol3 in line:
+                q = line.strip(strip)
+
+                l1 = q.split()
+                z = q[:24]
+                cort = (cday, l1[6])
+                l2.insert(4, l1[6])
+          #      print(z, l1[6], cday, cort)
+            if simvol4 in line:
+                q = line.strip(strip)
+                l1 = q.split()
+                z = q[:24]
+                cort = (cday, l1[7])
+                l2.insert(5, l1[7])
+         #       print(z, l1[7], cday, cort)
+        #print(l2)
+    return l2
+#-------------------------------------
+
+def filewriter(l):
+    ll = ' '.join(l)
+    print(ll)
+    f_name = "plan.txt"
+    with open(os.path.abspath(f_name), "a", encoding="utf-8") as f1:
+        f1.write(ll + "\n")
+        f1.close
     return
+
 #--------------------------------------
 print(os.getcwd())
 
-i  = fdata
+i = fdata
 print (sta, fdata, ldata)
-while(i < ldata):
+while(i < ldata+1):
     print(i)
     cday = i #current day
     if cday < 100:
         cday = "0"+str(cday)
+    """
     cmd01 = "wget -t 1  --ftp-user=petro-ew --ftp-password=fvbjksdx ftp://192.168.0.21/rinex/" + str(zz_year) + "/" + str(cday) + "/" +  sta + str(cday) + "0." + str(year_fn) + "d.Z"
     cmd02 = "wget -t 1  --ftp-user=petro-ew --ftp-password=fvbjksdx ftp://192.168.0.21/rinex/" + str(zz_year) + "/" + str(cday) + "/" +  sta + str(cday) + "0." + str(year_fn) + "g.Z"
     cmd03 = "wget -t 1  --ftp-user=petro-ew --ftp-password=fvbjksdx ftp://192.168.0.21/rinex/" + str(zz_year) + "/" + str(cday) + "/" +  sta + str(cday) + "0." + str(year_fn) + "n.Z"
@@ -56,36 +127,21 @@ while(i < ldata):
     os.chdir(some_dir)
     cmd04 = "/bin/uncompress /home/petro-ew/work/2013/*.Z"
     os.system(cmd04)
-    #execscr(cmd04)
     cmd05 = some_dir + "CRX2RNX " +  sta + str(cday) + "0." + str(year_fn) + "d"
     os.system(cmd05)
-    #execscr(cmd05)
     cmd06 = some_dir + "teqc +qc " +  sta + str(cday) + "0." + str(year_fn) + "o"
     execscr(cmd06)
     cmd_rmz = "rm " + some_dir + "*.Z " + some_dir + "*.azi" + some_dir + "*.n" + some_dir + "*.mp1" + some_dir + "*.mp2" + some_dir + "*.ion" + some_dir + "*.o" + some_dir + "*.g" + some_dir + "*.sn1" + some_dir + "*.sn2" + some_dir + "*.iod" + some_dir + "*.ele"
     execscr(cmd_rmz)
-    filename =
-    fileopen(, )
+    """
+    filename = some_dir + sta + str(cday) + "0." + str(year_fn) + "S"
+    filename = some_dir + "text\\"+ sta + str(cday) + "0." + str(year_fn) + "S"
+    l =  fileopen(filename, cday)
+    print(l)
+    filewriter(l)
 
     i+=1
 """
-j = fdata
-
-while (j < ldata):
-	os.chdir(some_dir)
-	cmd04 = "/bin/uncompress /home/petro-ew/work/2013/*.Z"
-	os.system(cmd04)
-	#execscr(cmd04)
-	cmd05 = some_dir + "CRX2RNX " +  sta + str(cday) + "0." + str(year_fn) + "d"
-	os.system(cmd05)
-	#execscr(cmd05)
-	cmd06 = some_dir + "teqc +qc " +  sta + str(cday) + "0." + str(year_fn) + "o"
-	execscr(cmd06)
-	cmd_rmz = "rm " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*d.Z " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*n " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*g " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*o " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*ion " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*iod " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*azi " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*ele " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*sn1 " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*sn2 " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*mp1 " + some_dir +  sta + str(cday) + "0." + str(year_fn) + "*mp2 "
-	execscr(cmd_rmz)
-
-	j+=1
-	"""
 os.system("/bin/chmod 777 *")
 cmd_rmz = "/bin/rm " + some_dir + "*.Z "
 os.system(cmd_rmz)
@@ -105,7 +161,6 @@ cmd_rmz ="/bin/rm " + some_dir +  "*." +   str(year_fn) + "g"
 os.system(cmd_rmz)
 cmd_rmz ="/bin/rm " + some_dir +   "*." +   str(year_fn) + "d"
 os.system(cmd_rmz)
-
 cmd_rmz ="/bin/rm " + some_dir + "*.sn1"
 os.system(cmd_rmz)
 cmd_rmz ="/bin/rm " + some_dir + "*.sn2"
@@ -114,4 +169,13 @@ cmd_rmz ="/bin/rm " + some_dir + "*.iod"
 os.system(cmd_rmz)
 cmd_rmz = "/bin/rm " + some_dir + "*.ele"
 os.system(cmd_rmz)
-#com  "some_dir2/day_kachalka.pl mend ".ARGV[0]." ".ARGV[1] 
+"""
+
+
+"""
+set term png
+set output "plot.png"
+set xtics 1
+plot [] [0:] "./plan.txt" using 1:2 with lines title "aaa", "./plan.txt" using 1:3 with lines title "bbb", "./plan.txt" using 1:4 with lines title "ccc"
+
+"""
