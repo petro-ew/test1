cf910be464e4081a3c1e4b230f03bcb36de4f6af
petro-ew <v.petroew@gmail.com>
2013-03-29 02:47:05 +0400

    matplota test commit

---
+++ solutions/sl-mp/grafics.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set fileencoding=utf-8 ts=4 sw=4 expandtab:
+__author__ = 'petro-ew'
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy
+import pylab
+import sys
+import string
+import matplotlib as mpl
+import matplotlib.figure
+
+#TITLE = sys.argv[1]
+TITLE = 'akko'
+# Create the figure (A4 format)
+plt.figure(num=None, figsize=(11.69, 8.27), dpi=300)
+fn = TITLE + ".slmp"
+print(fn)
+data1 = numpy.genfromtxt(TITLE + '.slmp', usecols = [1,])
+data2 = numpy.genfromtxt(TITLE + '.slmp', usecols = [2,])
+data3 = numpy.genfromtxt(TITLE + '.slmp', usecols = [3,])
+
+locs = np.arange(1, len(data1)+1)
+width = 0.27
+# Задаем заголовок диаграммы
+
+plt.title('Станция  ' + TITLE,{'fontname':'Arial','fontsize':16})
+#plt.title(u'AKKO')
+
+# Задаем подписи к осям X и Y
+
+#plt.xlabel(u'Дни с начала года')
+#plt.ylabel(u'Значения')
+plt.xlabel('Дни с начала года ',{'fontname':'Arial','fontsize':16})
+plt.ylabel('Значения ',{'fontname':'Arial','fontsize':16})
+
+# Включаем сетку
+
+plt.grid()
+
+plt.xticks(locs + width * 1.5, locs)
+pylab.bar(locs, data1, width = width, label = 'ls < 10')
+pylab.bar(locs + width, data2, width = width, color = 'red', label = 'ls > 10')
+pylab.bar(locs + 2 * width, data3, width = width, color = 'green', label = 'mp > 10')
+
+pylab.legend()
+#plt.savefig('spirit.png', format = 'png')
+#plt.show()
+#plt.savefig('spirit.pdf', format = 'pdf')
+# Save to PDF
+plt.savefig("%s.pdf" % TITLE)
